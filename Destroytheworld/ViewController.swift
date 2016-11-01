cdc221ceed0d9198341b9e09475d0b55d14564a5
Portal Services <portalservices@joshua-imac.local>
2016-11-02 00:03:44 +0100

    Initial Commit

---
+++ Destroytheworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Destroytheworld
+//
+//  Created by Joshua Balogun on 02/11/2016.
+//  Copyright © 2016 Etisalat Nigeria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
